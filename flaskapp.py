965af0af34dea04a45bc79569361a7d6258123f7
TailongCheng <157655951+TailongCheng@users.noreply.github.com>
2024-04-17 19:55:53 -0400

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -8,6 +8,5 @@ CORS(app)
 def hello_cloud():
   return 'Hello Cloud! From Tailong Cheng!'
 
-# Without port=8080, the docker container ip works, but GCP VM local ip and external ip doesnt work.
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
